256a07a8f701951e2df4b7c35b72637d31b6bb26
Surya V <suryasv0729@gmail.com>
2024-05-19 21:06:54 +0530

    making responsive

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ allLinks.forEach(function (link) {
     }
 
     // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
+    if (link.classList.contains("main-nav-list"))
       headerEl.classList.toggle("nav-open");
   });
 });
